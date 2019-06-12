b7d07aea9a3247eb388d53c584ff428808c3edc3
HarryCandlish <hcnd90@gmail.com>
2019-06-12 17:33:16 +1200

    updated matrix

--- matrix.js
+++ matrix.js
@@ -1,5 +1,18 @@
-function getMatrix (n) {
+function getMatrix(n) {
+  var matrix = new Array(n);
+  matrix.fill(new Array(n));
+  matrix.forEach(row => row.fill(0));
+  return matrix;
 }
 
-function updateMatrix (matrix, coords, value) {
+function updateMatrix(matrix, coords, value) {
+  var row = coords[0];
+  var col = coords[1];
+  matrix[row][col] = value;
+  return matrix;
 }
+
+module.exports = {
+  getMatrix,
+  updateMatrix
+};
