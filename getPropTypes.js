b48030cf47c564847c321dabb8693b9b6eaa1662
HarryCandlish <hcnd90@gmail.com>
2019-06-12 13:33:29 +1200

    getPropType

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,10 @@
-function getPropTypes (obj) {
+function getPropTypes(obj) {
+  var data = [];
+  var keys = Object.keys(obj);
+  for (let i = 0; 0 < keys.length; i++) {
+    data.push(typeof obj[keys[i]]);
+  }
+  return data;
 }
+
+module.exports = getPropTypes;
