7b5dd00433454b8711900330e6c8ea4e70c7dfad
HarryCandlish <hcnd90@gmail.com>
2019-06-12 17:13:23 +1200

    get first item of an array using shift, and get last item of an array using slice

--- positions.js
+++ positions.js
@@ -1,5 +1,12 @@
-function getFirst (arr) {
+function getFirst(arr) {
+  return arr.shift(arr);
 }
 
-function getLast (arr) {
+function getLast(arr) {
+  return arr.slice(-1)[0];
 }
+
+module.exports = {
+  getFirst,
+  getLast
+};
